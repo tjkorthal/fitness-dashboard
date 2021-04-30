3b5b58b5183d6748a7bd0047e4efb5e40ed6bb8b
Tyler Korthal <tjkorthal@live.com>
2021-04-29 23:21:12 -0400

    Add formatting methods for activity time

--- server/activity.rb
+++ server/activity.rb
@@ -2,14 +2,14 @@
 class Activity
   MILES_PER_METER = 0.00062137
   SECONDS_PER_HOUR = 3600
-  MINUTES_PER_HOUR = 60
+  SECONDS_PER_MINUTE = 60
 
   attr_reader :type, :distance, :elapsed_time, :moving_time
   def initialize(params)
     @type = params['type']
     @distance = params['distance']
-    @elapsed_time = params['elapsed_time']
-    @moving_time = params['moving_time']
+    @elapsed_time = params['elapsed_time'].to_f
+    @moving_time = params['moving_time'].to_f
     @start_time = params['start_date_local']
   end
 
@@ -18,11 +18,19 @@ class Activity
   end
 
   def elapsed_time_in_hours
-    elapsed_time.to_f / SECONDS_PER_HOUR
+    elapsed_time / SECONDS_PER_HOUR
+  end
+
+  def elapsed_time_to_human
+    time_to_human(elapsed_time / SECONDS_PER_MINUTE)
   end
 
   def moving_time_in_hours
-    moving_time.to_f / SECONDS_PER_HOUR
+    moving_time / SECONDS_PER_HOUR
+  end
+
+  def moving_time_to_human
+    time_to_human(moving_time / SECONDS_PER_MINUTE)
   end
 
   def start_time
@@ -33,9 +41,22 @@ class Activity
     {
       type: type,
       distance: format('%.2f mi', distance_in_miles),
-      elapsed_time: format('%.2f hr', elapsed_time_in_hours),
-      moving_time: format('%.2f hr', moving_time_in_hours),
+      elapsed_time_in_hours: elapsed_time_in_hours,
+      elapsed_time_to_human: elapsed_time_to_human,
+      moving_time_in_hours: moving_time_in_hours,
+      moving_time_to_human: moving_time_to_human,
       start_time: start_time
     }
+  end
+
+  private
+
+  def time_to_human(minutes)
+    hours = (minutes / 60).round(half: :down)
+    minutes = (minutes - hours * 60)
+    return format('%.0f hr', hours) if minutes <= 0
+    return format('%.0f min', minutes) if hours.zero?
+
+    format('%.0f hr %.0f min', hours, minutes)
   end
 end
